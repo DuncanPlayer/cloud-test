174e55c873abaf2beac146bd31610473e98006af
DuncanPlayer <1540014394@qq.com>
2020-10-14 23:27:14 +0800

    nacos-provider

---
+++ cloud-nacos-provider/src/main/java/com/china/cloud/nacos/CloudNacosApplication.java
@@ -0,0 +1,13 @@
+package com.china.cloud.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNacosApplication.class, args);
+    }
+
+}
---
+++ cloud-nacos-provider/src/test/java/com/china/cloud/nacos/CloudNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.china.cloud.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
