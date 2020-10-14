b83b99d769207b44a70acb76c8fb464de3790539
DuncanPlayer <1540014394@qq.com>
2020-10-14 23:08:00 +0800

    first commit

---
+++ cloud-xxl-job/src/test/java/com/china/could/CloudXxlJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.china.could;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudXxlJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
